18f71f575ccded7048cf542406b72e1fc1f00e26
Hanz <hanznikkolmaas@gmail.com>
2025-06-25 02:39:37 +0800

    side nav idea

--- src/components/main_components/navigation/SideNav.tsx
+++ src/components/main_components/navigation/SideNav.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import OverviewSVG from "../../icons/nav_icons/OverviewSVG"
 
 function SideNav() {
+  
+  type NavItem = {
+    label: string;
+    icon: React.ReactNode;
+  };
+
+  const navItems: NavItem[] = [
+    {label: "Overview", icon: <OverviewSVG></OverviewSVG>},
+    {label: "Overview", icon: <OverviewSVG></OverviewSVG>},
+    {label: "Overview", icon: <OverviewSVG></OverviewSVG>},
+    {label: "Overview", icon: <OverviewSVG></OverviewSVG>},
+  ]
   return (
     <>
-      <ul className='flex flex-col gap-4 text-lg'>
-        <li>Overview</li>
-        <li>Overview</li>
-        <li>Overview</li>
-        <li>Overview</li>
+      <ul className='flex flex-col gap-4'>  
+        {navItems.map((item, index) => (
+          <li 
+            key={index}
+            className="flex gap-4 items-center bg-gray-200 w-full py-3 px-4 rounded-lg">
+
+            {item.icon}
+            <h1>{item.label}</h1>
+
+          </li>
+        ))}
       </ul>
     </>
   )
--- src/pages/main/MainLayout.tsx
+++ src/pages/main/MainLayout.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
 import SideNav from '../../components/main_components/navigation/SideNav'
 
 function Main() {
   return (
     <>
     <div className='w-full min-h-screen flex justify-center items-center'>
-      <div className='w-[12%] h-full flex flex-col border-r border-r-gray-200 text-black p-4 gap-8'>
-        <div className='w-full text-center'>
-          <h1 className='text-3xl font-archivo text-primary'>Casha</h1>
+      {/* Side Nav */}
+      <div className='w-[12%] h-full flex flex-col justify-between border-r p-4 border-r-gray-200 text-black  bg-[#fcfcfc]'>
+        {/* Top */}
+        <div className='gap-6 flex flex-col'>
+          <div className='w-full text-center'>
+            <h1 className='text-3xl font-archivo text-primary'>Casha</h1>
+          </div>
+          <SideNav></SideNav>
         </div>
         
-        <SideNav></SideNav>
+        {/* Bottom */}
+        <div className='w-full'>
+          <button className='py-3 px-4 bg-red-logout w-full text-white rounded-lg'>Logout</button>
+        </div>
       </div>
-
+      {/* Main */}
       <div className='h-full flex-1 bg-off-white'>
 
       </div>
